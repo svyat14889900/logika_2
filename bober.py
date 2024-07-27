8bea8486a89b64c361db1e611d8cfeec736d6ff6
svyat14889900 <sviatyasni@gmail.com>
2024-07-27 15:34:38 +0300

    first file
    я зробив щось нове

---
+++ bober.py
@@ -0,0 +1,11 @@
+num_1 = int(input())
+num_2 = int(input())
+
+if num_1 > num_2:
+    print("перше число більше")
+elif num_2 >num_1:
+    print("Друге число більше")
+else:
+    print("числа рівні")
+
+print("кінець")
